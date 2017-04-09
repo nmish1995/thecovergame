f57566adf5cb7cb93999cea09f46d3d2e1513bff
nmish2005 <nmish2005@gmail.com>
2017-04-09 11:10:37 +0300

    ip push

---
+++ application/views/ip_view.php
@@ -0,0 +1,3 @@
+<?php
+$ip=$_SERVER["REMOTE_ADDR"];
+echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div>";
