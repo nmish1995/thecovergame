dd1308546001a0739c6a5b7c89253b0c1ab76521
nmish2005 <nmish2005@gmail.com>
2017-04-09 11:40:11 +0300

    ip_update push

--- application/views/ip_view.php
+++ application/views/ip_view.php
@@ -1,3 +1,8 @@
 <?php
-$ip=$_SERVER["REMOTE_ADDR"];
+include("/application/SxGeo.php");
+include("/application/SxGeoCity.php");
+//$SxGeo = new SxGeo(); // Режим по умолчанию, файл бд SxGeo.dat
+$SxGeo = new SxGeo('SxGeoCity.dat', SXGEO_BATCH | SXGEO_MEMORY); // Самый быстрый режим
+$SxGeo->get($ip);
+// $SxGeo->getCityFull($ip); (возвращает полную информацию о городе и регионе)
 echo "<div class='alert alert-warning' role='alert'><span class='glyphicon glyphicon-console' aria-hidden='true'></span>Ваш IP-адрес -- ".$ip."</div>";
