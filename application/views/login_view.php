da033168ad8faafdc3956b4230c23281a9445806
nmish2005 <nmish2005@gmail.com>
2017-04-10 22:57:59 +0300

    login push

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -33,21 +33,25 @@
 <?php
 
 //Отключаем вывод ошибок
-function err_handler($errno, $errmsg, $filename, $linenum)
-{
-    if (!in_array($errno, Array(E_NOTICE, E_STRICT, E_WARNING)))
-    {
+
+function err_handler($errno, $errmsg, $filename, $linenum){
+    if (!in_array($errno, Array(E_NOTICE, E_STRICT, E_WARNING))){
+
         $date = date('Y-m-d H:i:s (T)');
         $f = fopen('errors.log', 'a');
-        if (!empty($f)) { $err = "\r\n";
+        if (!empty($f)) {
+
+            $err = "\r\n";
             $err .= " $date\r\n";
             $err .= " $errno\r\n";
             $err .= " $errmsg\r\n";
             $err .= " $filename\r\n";
             $err .= " $linenum\r\n";
             $err .= "\r\n";
+
             fwrite($f, $err);
             fclose($f);
+
         }
     }
 }
@@ -55,30 +59,58 @@ set_error_handler('err_handler');
 
 require_once("start.php");
 
-if(isset($_POST["password"])){ $password = $_POST["password"]; }
-if(isset($_POST["login"])){ $login = $_POST["login"]; }
-if(isset($_POST["send"])){ $send = $_POST["send"]; }
+if(isset($_POST["password"])){
+
+    $password = $_POST["password"];
+
+}
+if(isset($_POST["login"])){
+
+    $login = $_POST["login"];
+
+}
+if(isset($_POST["send"])){
+
+    $send = $_POST["send"];
+
+}
+
 $login = mb_strtolower($login);
+
 $password = mb_strtolower($password);
+
 //удаляем лишние пробелы
+
 $login=trim($login);
+
 $password=trim($password);
+
 /* Проверяем если была нажата кнопка Войти. Если да, то сравниваем данные полученные из формы с тем логином и паролем который есть в БД и если они совпадаю то пользователь успешно авторизирован, иначе, выводим сообщение что неправильный логин или пароль. Если кнопка не была нажата, значит что пользователь зашел на страницу напрямую и поэтому выводим ему сообщение об этом. */
+
 if(isset($send)) {
+
     // делаем запрос к БД для выбора данных.
-    $query = " SELECT * FROM users WHERE login = $login AND password = '$password'";
+
+    $query = " SELECT * FROM `users` WHERE `login` = '{$login}' AND `password` = '{$password}'";
+
     $result = mysql_query($query) or die ("Error : " . mysql_error());
 
     if(!$result){
+
         exit ("<div class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-remove-sign' aria-hidden='true'></span>Логин и/или пароль введены неверно</div>");
+
     }else{
+
         $_SESSION['login'] = $result["login"];
         $_SESSION['id'] = $result["id"];
         $_SESSION['password'] = $result["password"];
         $_SESSION['name'] = $result["name"];
         $_SESSION['lastname'] = $result["lastname"];
+
         header("Location: /");
+
         exit;
     }
 }
+
 ?>
